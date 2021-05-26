06427ba1dbe1a2677d6fdfd2cfe04eae12b2cf20
mdshahnawazz <70908310+mdshahnawazz@users.noreply.github.com>
2021-05-26 18:01:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,30 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
 class App extends Component {
-    constructor(props) {
-		super(props);
-	    	this.state={isClicked:false};
-	};
-	printPara=()=>{
-		this.setState({isClicked:true})
-	}
 
-    render() {
-    	return(
-    		<div id="main">
-			<button id="click" onClick={this.printPara}>Click Me</button>
-				{ this.state.isClicked?<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null }
-			
+constructor(props) {
+    super(props);
+    this.state = {isClicked: false};
 
-    		</div>
-    	);
-    }
+
+ 
+
+// this.state = this.printPara.bind(this);
+
+
+ 
+
+  };
+  printPara = () => {
+	this.setState({isClicked: true})
+   }
+   render() {
+	return(
+		<div id="main">
+			<button id="click" onClick={this.printPara}>Click Me</button> - calling class function  with this
+			{this.state.isClicked ? <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>: null}
+		</div>
+	);
+   }
 }
-
-
 export default App;
-
